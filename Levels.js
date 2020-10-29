19e8e750ed747a27f5c145e6ee109f3518815383
Nghia.Bui <buitrungnghia1.11@gmail.com>
2020-10-29 13:56:06 +0700

    fixed position 1 so thu

--- Levels.js
+++ Levels.js
@@ -8,12 +8,12 @@ class Levels{
         i = this.id;
         let str = "";
         str += `
-        <div id="${i}" class="canvas-container" style="visibility: hidden">
-<!--        <img class="imgPannelBot" src="Img/General/password_panel.png" alt="">-->
-<!--        <img class="imgPannelTop" src="Img/General/top_panel.png" alt="">-->
-        <img class="imgButtonSubmit" onclick="submit(i)" src="Img/General/icon_submit.png" >
-        <input id="input${i}" type="text" placeholder="Enter the password">
-        <p class="deniedText" id="denied-text${i}" style="visibility: hidden" >ACCESS DENIED</p>
+        <div id="${i}" class="canvas-container" style="visibility: hidden" onclick="drag(event)">
+        <img class="imgPannelBot" src="Img/General/password_panel.png" alt="">
+        <img class="imgPannelTop" src="Img/General/top_panel.png" alt="">
+        <img class="imgButtonSubmit" onclick="submit(i)" onmouseover="zoomImage(this)" onmouseout="normalImage(this)" src="Img/General/icon_submit.png" >
+        <input class="input"id="input${i}" type="text" placeholder="Enter the password">
+        <p class="deniedText" id="denied-text${i}" style="visibility: hidden" >ACCESSDENIED</p>
         <p class="levelText" id="level-text${i}" >LEVEL</p>`
         switch (i){
             case 1 :
@@ -22,15 +22,15 @@ class Levels{
                 break;
             case 3 :
                 str += `<img  id="img-level${i}" class="imgLevel" src="Img/Level${i}.png" height="322" width="533"/>`;
-                str += `<img id="hint-button${i}"class="imgButtonHint" onclick="showHint()" src="Img/General/icon_next.png">`
+                str += `<img id="hint-button${i}"class="imgButtonHint" onclick="showHint()" onmouseover="zoomImage(this)" onmouseout="normalImage(this)" src="Img/General/icon_next.png">`
                 break;
             case 4 :
                 str += `<img  id="img-level${i}" class="imgLevel" src="Img/Level${i}.png" />`;
-                str += `<img id="hint-button${i}" class="imgButtonHint" onclick="showHint()" src="Img/General/icon_next.png">`
+                str += `<img id="hint-button${i}" class="imgButtonHint" onclick="showHint()" onmouseover="zoomImage(this)" onmouseout="normalImage(this)" src="Img/General/icon_next.png">`
                 break;
             case 5 :
                 for(let j = 1; j < 15; j++){
-                    str += `<img ondrag="" id="img-level${i}-${j}" class="imgLevel" src="Img/Level${i}-${j}.png" />`
+                    str += `<img onmousedown="getImageID(this.id)" onclick="drag(event)" onmouseup="drag(event)" id="img-level${i}-${j}" class="imgLevel" src="Img/Level${i}-${j}.png" />`
                 }
                 break;
         }
@@ -104,17 +104,35 @@ function changeLevelText(){
 function showHint(){
     let levelImg = document.getElementById("img-level" + i);
     let buttonHint = document.getElementById("hint-button" + i);
+    // var length = levelImg.src.length;
     let length = levels[i].hints.length;
-    for(let j = 0; j < length; j++){
-        if(levelImg.src.substring(28,levelImg.src.length) == levels[i].hints[j]){
+    for(let j = 0; j < levels[i].hints.length; j++){
+        if(levelImg.src.substring(levelImg.src.length - 14,levelImg.src.length) == levels[i].hints[j]){
             buttonHint.style.transform = "rotate(180deg)";
             levelImg.src = levels[i].hints[j + 1];
             break;
         }
-        if(levelImg.src.substring(28,levelImg.src.length) == levels[i].hints[length - 1]){
+        if(levelImg.src.substring(levelImg.src.length - 16,levelImg.src.length) == levels[i].hints[length - 1]){
             levelImg.src = levels[i].hints[0];
             buttonHint.style.transform = "rotate(0deg)";
             break;
         }
     }
 }
+function zoomImage(img){
+    img.style.height = "75px";
+    img.style.width = "75px";
+}
+function normalImage(img){
+    img.style.height = "50px";
+    img.style.width = "50px";
+}
+// function drag(event){
+//     console.log(event.offsetX);
+//     document.getElementById(imgID).style.left = event.offsetX + "px";
+//     document.getElementById(imgID).style.top = event.offsetY + "px";
+// }
+// let imgID;
+// function getImageID(imgId){
+//     imgID = imgId;
+// }
